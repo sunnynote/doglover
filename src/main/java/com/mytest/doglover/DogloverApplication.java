01d632074c9ffab46828098a56465d2176da7dcb
sunny <inseon0731@naver.com>
2023-02-26 17:19:08 +0900

    first commit

---
+++ src/main/java/com/mytest/doglover/DogloverApplication.java
@@ -0,0 +1,13 @@
+package com.mytest.doglover;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogloverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogloverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mytest/doglover/DogloverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mytest.doglover;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogloverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
