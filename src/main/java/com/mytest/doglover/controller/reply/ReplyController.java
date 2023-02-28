89eb9e558063b263805c4b8b6dd70f08a1c86822
sunny <inseon0731@naver.com>
2023-02-28 18:29:03 +0900

    feat: Reply 댓글 조회(Board 번호에 의한) API 구현

--- src/main/java/com/mytest/doglover/controller/reply/ReplyController.java
+++ src/main/java/com/mytest/doglover/controller/reply/ReplyController.java
@@ -1,15 +1,20 @@
 package com.mytest.doglover.controller.reply;
 
 import com.mytest.doglover.model.Board;
+import com.mytest.doglover.model.Reply;
 import com.mytest.doglover.model.user.User;
 import com.mytest.doglover.model.user.UserAccount;
 import com.mytest.doglover.service.BoardService;
 import com.mytest.doglover.service.ReplyService;
 import com.mytest.doglover.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @RestController
@@ -35,5 +40,20 @@ public class ReplyController {
     return replyService.write(replyRequest
             .newReply(board, user, replyRequest.getContent()))
             .getId();
+  }
+
+  @GetMapping("/{boardmapId}/{boardId}/reply")
+  public ResponseEntity<List<ReplyResponse>> read(@PathVariable("boardId") Long boardId){
+
+    Board board = boardService.findById(boardId)
+            .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 게시물입니다."));
+
+    List<Reply> replyList = replyService.findAllByBoard(board);
+
+    List<ReplyResponse> collect = replyList.stream()
+            .map(ReplyResponse::new)
+            .collect(Collectors.toList());
+
+    return ResponseEntity.ok(collect);
   }
 }
--- src/main/java/com/mytest/doglover/service/ReplyService.java
+++ src/main/java/com/mytest/doglover/service/ReplyService.java
@@ -1,9 +1,12 @@
 package com.mytest.doglover.service;
 
+import com.mytest.doglover.model.Board;
 import com.mytest.doglover.model.Reply;
 import com.mytest.doglover.repository.ReplyRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 public class ReplyService {
@@ -16,5 +19,10 @@ public class ReplyService {
   @Transactional
   public Reply write(Reply reply){
     return replyRepository.save(reply);
+  }
+
+  @Transactional
+  public List<Reply> findAllByBoard(Board board){
+    return replyRepository.findAllByBoard(board);
   }
 }
